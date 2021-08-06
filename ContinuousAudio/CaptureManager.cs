ba6af8a6ee44862d23c3cbbc037d2649154ef549
Daniel Guceanu - Internships 2021 <guceanudaniel18@gmail.com>
2021-08-06 12:13:21 +0300

    Update first commit

--- ContinuousAudio/CaptureManager.cs
+++ ContinuousAudio/CaptureManager.cs
@@ -12,21 +12,21 @@ using System.Text;
 using System.Threading;
 using Windows.ApplicationModel.Core;
 using static System.Net.Mime.MediaTypeNames;
-using NAudio.Wave;
 
 namespace ContinuousAudio
 {
     internal class CaptureManager
-    {
+    {   
+        //declararea tuturor variabilelor cu care lucram
         public DisplayRequest displayRequest = new DisplayRequest();
         public bool isInitialized;
         public bool isRecording;
-        private StorageFolder captureFolder = null;
         public static MemoryStream memStream = new MemoryStream();
         public IRandomAccessStream stream = memStream.AsRandomAccessStream();
 
         MediaCapture captureManager;
 
+        //initializeaza microfonul
         public async Task InitializeCameraAsync()
         {
             captureManager = new MediaCapture();
@@ -38,26 +38,19 @@ namespace ContinuousAudio
             }
         }
 
+        //porneste inregistrarea
         public async Task StartRecordingAudioAsync()
         {
-
-            /*var picturesLibrary = await StorageLibrary.GetLibraryAsync(KnownLibraryId.Pictures);
-            captureFolder = picturesLibrary.SaveFolder;
-            //captureFolder = @"C:\Users\40731\source\repos\ConnectToWebSocket\ConnectToWebSocket\bin\Debug\netcoreapp3.1";
-            var audioFile = await captureFolder.CreateFileAsync("guc.wav", CreationCollisionOption.GenerateUniqueName);*/
             var encodingProfile = MediaEncodingProfile.CreateWav(AudioEncodingQuality.Medium);
             encodingProfile.Audio = AudioEncodingProperties.CreatePcm(16000, 1, 16);
-            //Debug.WriteLine("Starting recording to " + audioFile.Path);
-
-            //await captureManager.StartRecordToStorageFileAsync(encodingProfile, audioFile);
 
             await captureManager.StartRecordToStreamAsync(encodingProfile, stream);
 
             Debug.WriteLine("Started recording!");
             isRecording = true;
-            
         }
 
+        //opreste inregistrarea si transmite stream-ul catre serverul live-transcriber
         public async Task StopRecordingAudioAsync()
         {
             isRecording = false;
@@ -66,7 +59,8 @@ namespace ContinuousAudio
             Debug.WriteLine("Stopped recording!");
             
         }
-
+        
+        //realizeaza pasii necesari conectarii la server prin websocket
         public async Task speechtotext(Uri uri)
         {
 
